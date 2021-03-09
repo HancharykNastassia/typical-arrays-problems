cb336d8f6eff465e38fe20901e9532f3cdd3fad3
Nastassia Hancharyk <goncharik.anastasia@mail.ru>
2021-03-09 16:40:47 +0300

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (array !== undefined && array.length !=0) ? array.sort((a,b)=>a-b)[0] : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array !== undefined && array.length !=0) ? array.sort((a,b)=>a-b)[array.length - 1] : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length ==0) return 0;
+  let res=0;
+  array.map(item=>res = res + item);
+  return res/array.length;
 }
